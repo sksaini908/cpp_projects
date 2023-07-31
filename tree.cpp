25c76290748a6e65fb81ff02e05c751211b90f32
sksaini908 <sksaini908@gmail.com>
2023-07-31 15:39:43 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,7 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>>
 
 using namespace std;
 
+struct bst {
+  int val;
+  struct bst *llink;
+  struct bst *rlink;
+};
+
+struct bst *createNode(int val) {
+  struct bst * node  = new bst();
+  node->val = val;
+  node->llink = NULL;
+  node->rlink = NULL;
+}
+
 int main() {
-  std::cout << "kumar saini" << std::endl;
+  struct bst *root = createNode(10);
+  root->llink = createNode(20);
+  std::cout << root->llink->val << std::endl;
+  return 0;
 }
